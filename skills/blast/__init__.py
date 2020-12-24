733a380acd10c7ce5bb73e340579fe5cf7337388
Rex Roof <rexroof@gmail.com>
2020-12-24 12:59:18 -0500

    made filename configuration option

--- skills/blast/__init__.py
+++ skills/blast/__init__.py
@@ -1,20 +1,24 @@
 from opsdroid.skill import Skill
 from opsdroid.matchers import match_regex, match_always
+from voluptuous import Required
 import logging
 
-FILENAME = "/usr/src/app/blastfile.txt"
+CONFIG_SCHEMA = {
+    Required("filename", default="/usr/src/app/blastfile.txt"): str,
+}
 
 class BlastSkill(Skill):
 
     def __init__(self, opsdroid, config):
         super(BlastSkill, self).__init__(opsdroid, config)
-        with open(FILENAME, 'w') as f:
+        self.filename = config["filename"]
+        with open(self.filename, 'w') as f:
             f.write('!blast');
         logging.debug(f'Loaded blast skill')
 
     @match_regex(r'^!blast')
     async def blast(self, message):
-        with open(FILENAME, 'w') as f:
+        with open(self.filename, 'w') as f:
             f.write(message.user);
         logging.debug(f'updated blastfile with {message.user}')
         await message.respond(f'updated blastfile with {message.user}')
