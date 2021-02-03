99ea0f91017d42f1d0f584f02db07f4cf8454d6d
Rex Roof <rexroof@gmail.com>
2021-02-03 11:47:22 -0500

    updated popup skill, better logging, more generic function to generate popups

--- skills/popup/__init__.py
+++ skills/popup/__init__.py
@@ -2,30 +2,51 @@ from opsdroid.skill import Skill
 from opsdroid.matchers import match_regex, match_always
 import logging
 import subprocess
-import re
 
+import re
 
 class Popup(Skill):
     def __init__(self, opsdroid, config):
         super(Popup, self).__init__(opsdroid, config)
         logging.debug(f"Loaded popup skill")
 
-    @match_regex(r"^!popup")
-    async def simple_popup(self, message):
+    async def tmux_popup(self, cmd):
         completed = subprocess.run(["su", "rex", "-c", 
             "tmux list-clients"
             ], capture_output=True)
         stdout = completed.stdout.decode('utf-8')
+        stderr = completed.stderr.decode('utf-8')
+        retcode = completed.returncode
+        # check retcode, print stderr.
+
+        if retcode != 0:
+            logging.warning(f"retcode is {retcode}")
+
+        if stderr:
+            logging.debug(f"stderr is {stderr}")
+
         # grabbing the pty for twitch client
         matches = re.search('^(/[^:]+): (?=twitch.)', stdout, re.MULTILINE)
         pty = matches.group(0)
-        subprocess.run(
+
+        completed = subprocess.Popen(
             [
                 "su",
                 "rex",
                 "-c",
-                # f"tmux popup -c {pty} -w95 -h6 -K -R 'toilet -t -f future $(date)'",
-                f"tmux popup -c {pty} -K -R 'toilet -t -f future $(date)'",
+                f"tmux popup -c {pty} -w 80% -h 80% -K -R '{cmd}'"
             ]
         )
-        await message.respond(f"generated a popup!")
+        return completed.returncode
+
+    @match_regex(r"^#\s*aquarium")
+    async def aquarium(self, message):
+        await self.tmux_popup("docker run --rm -it wernight/funbox asciiquarium")
+
+    @match_regex(r"^#\s*(kubectl|k) get pod")
+    async def kgetpods(self, message):
+        await self.tmux_popup("kubectl get pods")
+
+    @match_regex(r"^#\s*popup date")
+    async def popup_date(self, message):
+        await self.tmux_popup( "toilet -t -f future $(date)" )
