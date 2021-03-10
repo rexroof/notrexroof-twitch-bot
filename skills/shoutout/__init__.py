03978157e1008f7dda7836d7c7c12b4878df39ae
Rex Roof <rexroof@gmail.com>
2021-03-10 16:19:45 -0500

    updated shoutout to auto-create new streamer in memory, default message

--- skills/shoutout/__init__.py
+++ skills/shoutout/__init__.py
@@ -21,40 +21,42 @@ class ShoutOut(Skill):
         super(ShoutOut, self).__init__(opsdroid, config)
         logging.debug(f"Loaded ShoutOut skill")
 
+    def new_streamer(self, obj, username):
+        if obj is None:
+            obj = {}
+        if username not in obj:
+            obj[username] = {"msg": "", "count": 0}
+        return obj
+
     @match_regex(r"^#\s*(soadd|addso) (?P<streamer>[\w]+) (?P<response>.+)$")
     @constrain_users(["rexroof"])
     async def shoutout_add(self, message):
         streamer = message.regex.group("streamer").lower()
         response = message.regex.group("response")
         shoutout = await self.opsdroid.memory.get("shoutout")
-        if shoutout == None:
-            shoutout = {}
+        shoutout = self.new_streamer(shoutout, streamer)
 
-        if streamer in shoutout:
-            await message.respond(
-                f"replacing shoutout for https://twitch.tv/{streamer}"
-            )
-            shoutout[streamer] = {"msg": response, "count": shoutout[streamer]["count"]}
-        else:
-            await message.respond(
-                f"creating new shoutout for https://twitch.tv/{streamer}"
-            )
-            shoutout[streamer] = {"msg": response, "count": 0}
+        shoutout[streamer]["msg"] = response
+        await message.respond(f"replaced shoutout for https://twitch.tv/{streamer}")
         await self.opsdroid.memory.put("shoutout", shoutout)
 
     @match_regex(r"^#\s*(shoutout|so) (?P<streamer>[\w]+)")
     async def print_shoutout(self, message):
         streamer = message.regex.group("streamer").lower()
         shoutout = await self.opsdroid.memory.get("shoutout")
-        if streamer in shoutout:
-            stmr = shoutout[streamer]
-            stmr["count"] += 1
-            s = "s"
-            if stmr["count"] == 1:
-                s = ""
+        shoutout = self.new_streamer(shoutout, streamer)
+
+        stmr = shoutout[streamer]
+        stmr["count"] += 1
+        s = "s"
+        if stmr["count"] == 1:
+            s = ""
+        if stmr["msg"]:
             await message.respond(
                 f'rex says: {stmr["msg"]} https://twitch.tv/{streamer} ({streamer} has been shouted out {stmr["count"]} time{s})'
             )
-            await self.opsdroid.memory.put("shoutout", shoutout)
         else:
-            await message.respond(f"no shoutout recorded for {streamer}!")
+            await message.respond(
+                f'rex says: if you aren\'t watching and following {streamer}, what are you even doing?  https://twitch.tv/{streamer} ({streamer} has been shouted out {stmr["count"]} time{s})'
+            )
+        await self.opsdroid.memory.put("shoutout", shoutout)
