37dbe78a5270285365472fa09876e01b4d33ca6c
Rex Roof <rexroof@gmail.com>
2021-03-24 08:32:19 -0400

    turned replies into objects with formatting

--- skills/reply/__init__.py
+++ skills/reply/__init__.py
@@ -18,7 +18,46 @@ class Reply(Skill):
                 token = mat.group("token")
                 replies = await self.opsdroid.memory.get("replies")
                 if replies != None and token in replies:
-                    await message.respond(f"{replies[token]}")
+                    # running this to possibly convert this item
+                    replies = self.new_reply(replies, token)
+
+                    replies[token]["count"] += 1
+                    text = replies[token]["text"]
+
+                    _user = message.user
+                    _count = replies[token]["count"]
+
+                    try:
+                        result = text.format(user=_user, count=_count)
+                    except KeyError as e:
+                        logging.debug(f"reply keyerror {e}")
+                        result = text
+                    except ValueError as e:
+                        logging.debug(f"reply valueerror {e}")
+                        result = text
+                    except Exception as e:
+                        logging.debug(f"uncaught reply error {e}")
+                        result = text
+
+                    # add formats
+                    #  - username
+                    #  - counter
+                    #  - arg to command? $1 & $2 ?
+                    # add multiple replies, randomly choose between each
+
+                    await self.opsdroid.memory.put("replies", replies)
+                    await message.respond(result)
+
+    def new_reply(self, replies, key):
+        if replies == None:
+            replies = {}
+        if key not in replies:
+            replies[key] = {"count": 0, "text": ""}
+
+        # add block to convert
+        if isinstance(replies[key], str):
+            replies[key] = {"count": 0, "text": replies[key]}
+        return replies
 
     @match_regex(r"^#\s*reply to #(?P<key>[\w]+) with (?P<response>.+)$")
     @constrain_users(["rexroof"])
@@ -26,9 +65,8 @@ class Reply(Skill):
         key = message.regex.group("key")
         response = message.regex.group("response")
         replies = await self.opsdroid.memory.get("replies")
-        if replies == None:
-            replies = {}
-        replies[key] = response
+        replies = self.new_reply(replies, key)
+        replies[key]["text"] = response
         await self.opsdroid.memory.put("replies", replies)
         await message.respond(f'wrote "{key}" to memory')
 
